83b4c4dcee96004fe553635bdabecf7621159b50
stroich <stroichmwork@gmail.com>
2023-10-29 23:27:43 +0400

    refactor: add MainPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,9 @@
 import { Component } from 'react';
-import Search from './components/header/search.tsx';
-import ListOfCard from './components/listOfCard/listOfCard.tsx';
-import { getArrArtWork, IArtwork } from './components/api.ts';
-import { ErrorBoundary } from './components/Error/ErrorBoundary.tsx';
-import { ErrorButton } from './components/ErrorButton/ErrorButton.tsx';
+import MainPage from './MainPage/MainPage.tsx';
 
-interface AppState {
-  arrValue: Array<IArtwork>;
-  isLoading: boolean;
-}
-
-class App extends Component<NonNullable<unknown>, AppState> {
-  constructor(props: NonNullable<unknown>) {
-    super(props);
-    this.state = {
-      arrValue: [],
-      isLoading: false,
-    };
-  }
-
-  setArrValue = async () => {
-    this.setState(() => ({
-      isLoading: true,
-    }));
-    const searchValue = localStorage.getItem('searchValue');
-    const queryValue = searchValue || ' ';
-    const arrValue = await getArrArtWork(queryValue);
-    this.setState({
-      arrValue: arrValue,
-      isLoading: false,
-    });
-  };
-
-  async componentDidMount() {
-    await this.setArrValue();
-  }
-
+class App extends Component {
   render() {
-    return (
-      <div className={'container'}>
-        <Search setArrValue={this.setArrValue} />
-        <ErrorBoundary>
-          <main>
-            {!this.state.isLoading && (
-              <ListOfCard artworks={this.state.arrValue} />
-            )}
-            {this.state.isLoading && <div className="loading">loading...</div>}
-          </main>
-          <ErrorButton />
-        </ErrorBoundary>
-      </div>
-    );
+    return <MainPage />;
   }
 }
 
---
+++ src/MainPage/MainPage.tsx
@@ -0,0 +1,59 @@
+import { Component } from 'react';
+import { getArrArtWork, IArtwork } from '../API/api.ts';
+import Search from '../components/SearchSection/search.tsx';
+import { ErrorBoundary } from '../components/Error/ErrorBoundary.tsx';
+import ListOfCard from '../components/listOfCard/listOfCard.tsx';
+import { ErrorButton } from '../components/ErrorButton/ErrorButton.tsx';
+import './MainPage.css';
+
+interface MainState {
+  arrValue: Array<IArtwork>;
+  isLoading: boolean;
+}
+
+class MainPage extends Component<object, MainState> {
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      arrValue: [],
+      isLoading: false,
+    };
+  }
+
+  updateData = async () => {
+    this.setState(() => ({
+      isLoading: true,
+    }));
+    const searchValue = localStorage.getItem('searchValue');
+    const queryValue = searchValue || ' ';
+    const arrValue = await getArrArtWork(queryValue);
+    this.setState({
+      arrValue: arrValue,
+      isLoading: false,
+    });
+  };
+
+  async componentDidMount() {
+    await this.updateData();
+  }
+
+  render() {
+    return (
+      <div className={'container'}>
+        <Search setArrValue={this.updateData} />
+        <ErrorBoundary>
+          <main>
+            {this.state.isLoading ? (
+              <div className="loading">loading...</div>
+            ) : (
+              <ListOfCard artworks={this.state.arrValue} />
+            )}
+          </main>
+          <ErrorButton />
+        </ErrorBoundary>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
