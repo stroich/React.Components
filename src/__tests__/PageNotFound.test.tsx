7ed47ce3dcd596cd7f32c640ce21d74aedf1e224
stroich <stroichmwork@gmail.com>
2023-11-10 20:44:55 +0400

    test: add the test

---
+++ src/__tests__/PageNotFound.test.tsx
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+
+import PageNotFound from '../components/404/PageNotFound.tsx';
+import Loading from '../components/Loading/Loading.tsx';
+
+test('404 page is displayed when navigating to an invalid route', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={['/non-existent-route']}>
+      <Routes>
+        <Route path="*" element={<PageNotFound />} />
+        <Route path={'/'} element={<Loading />} />
+      </Routes>
+    </MemoryRouter>
+  );
+  const pageTitle = getByText('404 - Page Not Found');
+  expect(pageTitle).toBeInTheDocument();
+});
