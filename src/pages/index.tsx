9dbc27691ce70f068fb0a6de90025e3b6b1bca95
stroich <stroichmwork@gmail.com>
2023-11-22 22:58:26 +0400

    refactor: refactor files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import { Inter } from 'next/font/google';
 import Head from 'next/head';
 
-import styles from '@/styles/Home.module.css';
-
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
@@ -13,7 +11,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}></main>
+      <main className={`${inter.className}`}></main>
     </>
   );
 }
