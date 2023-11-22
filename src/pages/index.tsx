8685530b46224363ce723829fb6057220cff9890
stroich <stroichmwork@gmail.com>
2023-11-22 22:52:02 +0400

    ci: migration next.js

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+
+import styles from '@/styles/Home.module.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Works of art</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${styles.main} ${inter.className}`}></main>
+    </>
+  );
+}
